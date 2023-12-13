911c86ff8d9484ec19b003f0dceaac08627db3f2
Sydney Berry <sjberry@princeton.edu>
2023-12-12 21:13:07 -0500

    quote tokens implemented

--- shell.go
+++ shell.go
@@ -38,23 +38,42 @@ func execInput(input string) error {
 	input = strings.TrimSuffix(input, "\n")
 
 	// Split the input separate the command and the arguments.
-	args := strings.Split(input, " ")
+	args := strings.Split(input, "\"")
+	var args2 []string
+	for i := range args {
+		if (i % 2) != 1 {
+			// fmt.Println("case 1")
+			// fmt.Println(args[i])
+			args[i] = strings.TrimSuffix(args[i], " ")
+			args[i] = strings.TrimPrefix(args[i], " ")
+			split := strings.Split(args[i], " ")
+			for j := range split {
+				args2 = append(args2, split[j])
+			}
+		} else {
+			// fmt.Println("case 2")
+			// fmt.Println(args[i])
+			args2 = append(args2, args[i])
+		}
+	}
+
+	// fmt.Println(args2)
 
 	// Check for built-in commands.
-	switch args[0] {
+	switch args2[0] {
 	case "cd":
 		// 'cd' to home with empty path not yet supported.
-		if len(args) < 2 {
+		if len(args2) < 2 {
 			return ErrNoPath
 		}
 		// Change the directory and return the error.
-		return os.Chdir(args[1])
+		return os.Chdir(args2[1])
 	case "exit":
 		os.Exit(0)
 	}
 
 	// Prepare the command to execute.
-	cmd := exec.Command(args[0], args[1:]...)
+	cmd := exec.Command(args2[0], args2[1:]...)
 
 	// Set the correct output device.
 	cmd.Stderr = os.Stderr
