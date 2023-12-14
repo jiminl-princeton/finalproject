3c60fdbd27845be9f3bdefdefdc4d92383bd5726
Jimin Lee <jiminl@princeton.edu>
2023-12-13 21:16:30 -0500

    implement functions with Sydney

--- shell.go
+++ shell.go
@@ -23,7 +23,7 @@ func main() {
 		}
 
 		// Handle the execution of the input.
-		if err = execInput(input); err != nil {
+		if err := execInput2(input); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
 	}
@@ -31,6 +31,101 @@ func main() {
 
 // ErrNoPath is returned when 'cd' was called without a second argument.
 var ErrNoPath = errors.New("path required")
+
+var ErrInvalidCommand = errors.New("invalid command")
+
+var lastArgs = 0
+
+func writeToDirectory(source, target string) {
+	// https://stackoverflow.com/questions/56075774/golang-os-renamefromdir-todir-not-working-in-windows
+	origFile, _ := os.ReadFile(source)
+	newFile, _ := os.Create(target + "/" + source)
+	fmt.Fprintf(newFile, "%s", string(origFile))
+}
+
+func writeToFile(source, target string) error {
+	// https://freshman.tech/snippets/go/check-if-file-is-dir/
+	// source path (must exist)
+	_, err := os.Stat(source)
+	if err != nil {
+		return ErrNoPath
+	}
+	// target path (need not exist)
+	_, err = os.Stat(target)
+	if err != nil {
+		os.Create(target)
+	} else {
+		os.Remove(target)
+	}
+	// https://stackoverflow.com/questions/56075774/golang-os-renamefromdir-todir-not-working-in-windows
+	origFile, _ := os.ReadFile(source)
+	newFile, _ := os.Create(target + "/" + source)
+	fmt.Fprintf(newFile, "%s", string(origFile))
+	return nil
+}
+
+func redirectIO(args []string) error {
+	redirectInput := -1  // index of < symbol
+	redirectOutput := -1 // index of > symbol
+	redirectInputSeen := false
+	redirectOutputSeen := false
+
+	// check if there is more than one of the same redirection symbol
+	for i, e := range args {
+		if e == "<" {
+			if !redirectInputSeen {
+				redirectInputSeen = true
+				redirectInput = i
+			} else {
+				return ErrInvalidCommand
+			}
+		} else if e == ">" {
+			if !redirectOutputSeen {
+				redirectOutputSeen = true
+				redirectOutput = i
+			} else {
+				return ErrInvalidCommand
+			}
+		}
+	}
+
+	if redirectInput != -1 {
+		if redirectInput+1 >= len(args) || redirectInput-1 < 0 {
+			return ErrNoPath
+		}
+		source := args[redirectInput+1]
+		target := args[redirectInput-1]
+		writeToFile(source, target)
+	}
+
+	if redirectOutput != -1 {
+		if redirectInput+1 >= len(args) || redirectInput-1 < 0 {
+			return ErrNoPath
+		}
+		source := args[redirectInput-1]
+		target := args[redirectInput+1]
+		writeToFile(source, target)
+	}
+
+	return nil
+}
+
+func execInput2(input string) error {
+	if err := execInput2(input); err != nil {
+		return err
+	}
+	args := strings.Split(input, " ")
+	if lastArgs < len(args)-1 {
+		if args[lastArgs+1] == "&&" {
+			input2 := ""
+			for i := lastArgs + 2; i < len(args); i++ {
+				input2 = input2 + args[i]
+			}
+			return execInput2(input2)
+		}
+	}
+	return nil
+}
 
 func execInput(input string) error {
 	// Remove the newline character.
@@ -42,6 +137,7 @@ func execInput(input string) error {
 	// Check for built-in commands.
 	switch args[0] {
 	case "cd":
+		lastArgs = 1
 		// 'cd' to home with empty path not yet supported.
 		if len(args) < 2 {
 			return ErrNoPath
@@ -49,17 +145,20 @@ func execInput(input string) error {
 		// Change the directory and return the error.
 		return os.Chdir(args[1])
 	case "pwd":
+		lastArgs = 0
 		// Get working directory and error
 		wd, err := os.Getwd()
 		fmt.Println(wd)
 		return err
 	case "mkdir":
+		lastArgs = 1
 		if len(args) < 2 {
 			return ErrNoPath
 		}
 		// Make the directory and return the error.
 		return os.Mkdir(args[1], os.ModePerm) // double check ModePerm
 	case "mv":
+		lastArgs = 2
 		if len(args) < 3 {
 			return ErrNoPath
 		}
@@ -72,26 +171,27 @@ func execInput(input string) error {
 		if !info.IsDir() {
 			return os.Rename(args[1], args[2])
 		}
-		// https://stackoverflow.com/questions/56075774/golang-os-renamefromdir-todir-not-working-in-windows
-		origFile, _ := os.ReadFile(args[1])
-		newFile, _ := os.Create(args[2] + "/" + args[1])
-		fmt.Fprintf(newFile, "%s", string(origFile))
+		writeToDirectory(args[1], args[2])
 		os.Remove(args[1])
 		return nil
 	case "rename":
+		lastArgs = 2
 		if len(args) < 3 {
 			return ErrNoPath
 		}
 		return os.Rename(args[1], args[2])
 	case "rm":
+		lastArgs = 1
 		if len(args) < 2 {
 			return ErrNoPath
 		}
 		return os.Remove(args[1])
 	case "getpid":
+		lastArgs = 0
 		fmt.Println(os.Getpid())
 		return nil
 	case "setenv":
+		lastArgs = 1
 		if len(args) < 2 {
 			return ErrNoPath
 		}
@@ -100,6 +200,7 @@ func execInput(input string) error {
 		pair := strings.Split(args[1], "=")
 		return os.Setenv(pair[0], pair[1])
 	case "getenv":
+		lastArgs = 1
 		if len(args) < 2 {
 			return ErrNoPath
 		}
@@ -110,11 +211,13 @@ func execInput(input string) error {
 		fmt.Println(value)
 		return nil
 	case "unset":
+		lastArgs = 1
 		if len(args) < 2 {
 			return ErrNoPath
 		}
 		return os.Unsetenv(args[1])
 	case "echo":
+		lastArgs = 1
 		if len(args) < 2 {
 			fmt.Println()
 			return nil
@@ -123,6 +226,7 @@ func execInput(input string) error {
 		fmt.Println(split[1])
 		return nil
 	case "ls":
+		lastArgs = 0
 		// https://stackoverflow.com/questions/14668850/list-directory-in-go
 		entries, err := os.ReadDir("./")
 		if err != nil {
@@ -135,6 +239,10 @@ func execInput(input string) error {
 	case "cat":
 		// not working
 		// https://gist.github.com/tetsuok/2795749#file-cat-go-L53
+		lastArgs = 1
+		if len(args) < 2 {
+			return ErrNoPath
+		}
 		for i := 1; i < len(args); i++ {
 			file, err := os.Open(args[i])
 			if err != nil {
@@ -156,12 +264,14 @@ func execInput(input string) error {
 		}
 		return nil
 	case "kill":
+		lastArgs = 0
 		process, err := os.FindProcess(os.Getpid())
 		if err != nil {
 			return err
 		}
 		return process.Kill()
 	case "exit":
+		lastArgs = 0
 		os.Exit(0)
 		return nil
 	}
